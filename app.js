a7af23607767801a1552d89bb6b037b7ade68b9a
Duyile Tolulope Patrick <princericky2000@yahoo.com>
2017-04-23 14:01:34 +0100

    design template with side bar

---
+++ app.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+	// Initialize collapse button
+    $(".button-collapse").sideNav();
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    //$('.collapsible').collapsible();
+        
+});
+ 
